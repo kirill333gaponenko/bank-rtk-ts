3cfbcfcffbafc8a4dffc070d4578e57a332a56c5
kirill <kirill56gaponenko@gmail.com>
2026-01-16 04:31:15 +0200

    first commit

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,14 +1,16 @@
 import type {RootState} from "../app/store.ts";
 import {useAppSelector} from "../app/hooks.ts";
 import Bank from "./Bank.tsx";
-import {useMemo} from "react";
-import {getRandomIndex, names} from "../utils/constants.ts";
+import {getBankName, getRandomIndex, names} from "../utils/constants.ts";
+import {useCallback} from "react";
 
 const Balance = () => {
     const balance = useAppSelector<RootState, number>(state => state.balance);
     const quote = useAppSelector<RootState, string>(state => state.quote);
     const index = getRandomIndex(names.length);
-    const info =useMemo(() => ({name:names[index]}),[index]);
+    const info = useCallback((index:number) =>getBankName(index),[])
+
+
 
     // const [info] = useState({name:'Braavos'})
 
@@ -16,10 +18,10 @@ const Balance = () => {
 
     // const info = useRef({name:'Braavos'})
 
-    console.log(`Render Balance `)
+    console.log(`Render Balance ${index}`)
     return (
         <div className={'text-center text-uppercase'}>
-            <Bank info={info}/>
+            <Bank info={info} index={index}/>
             <h3 className={'text-5xl text-[whitesmoke]'}>{quote}</h3>
             <h2 className={'text-7xl text-[firebrick]'}>Balance = {balance}</h2>
         </div>
--- src/components/Bank.tsx
+++ src/components/Bank.tsx
@@ -1,12 +1,14 @@
 import {memo} from "react"
 
 interface BankProps{
-    info: {name:string};
+    index:number,
+    info: (index: number) => ({name:string});
+
 }
-const Bank = ({info}:BankProps) => {
-    console.log(`Render Balance ${info.name}`)
+const Bank = ({info,index}:BankProps) => {
+    console.log(`Render Balance ${info(index).name}`)
     return (
-            <h1 className={'font-bold text-8xl text-[gold]'}>Iron Bank of {info.name}</h1>
+            <h1 className={'font-bold text-8xl text-[gold]'}>Iron Bank of {info(index).name}</h1>
     )
 }
 
